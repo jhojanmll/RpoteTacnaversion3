a9236499a45a02802e3b37d208053afa3323f71f
jhojan M-Ll <jhojanmll@gmail.com>
2018-09-06 00:39:31 -0500

    mejora de mapas

--- app/src/main/java/com/example/jimenez/appmunitacna/MapsActivity.java
+++ app/src/main/java/com/example/jimenez/appmunitacna/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.jimenez.appmunitacna;
 
 import android.Manifest;
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -9,10 +10,13 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -29,14 +33,17 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
     private Marker marcador;
+    private Marker destino;
     double lat = 0.0;
     double lng = 0.0;
     String mensaje1;
     String direccion = "";
+    String direcciondestino = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +53,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button boton1 = (Button)findViewById(R.id.boton1);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         miUbicacion();
-
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Location temp = new Location(LocationManager.GPS_PROVIDER);
+                temp.setLatitude(latLng.latitude);
+                temp.setLongitude(latLng.longitude);
+                setLocationdest(temp);
+                if (destino != null) destino.remove();
+                    destino = mMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title("Dirección:" + direcciondestino));
+            }
+        });
 
 
     }
@@ -83,6 +104,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+
+    public void setLocationdest(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+                    direcciondestino = (DirCalle.getAddressLine(0));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     private void AgregarMarcador(double lat, double lng) {
         LatLng coordenadas = new LatLng(lat, lng);
         CameraUpdate MiUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
@@ -101,6 +140,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
     }
+
+
+
     LocationListener locListener = new LocationListener() {
 
         @Override
@@ -147,12 +189,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    public void Mensaje() {
+    public void Mensaje(){
         Toast toast = Toast.makeText(this, mensaje1, Toast.LENGTH_LONG);
         //toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
         toast.show();
     }
 
-}
+    public void botonuno(View view){
+
+        Intent intentl=new Intent(MapsActivity.this,MandarReporteActivity.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Bundle bundle = ActivityOptions.makeSceneTransitionAnimation(MapsActivity.this).toBundle();
+            intentl.putExtra("direcciondestino","direcciondestino");
+            startActivity(intentl, bundle);
+        }else{
+
+            startActivity(intentl);
+        }
 
 
+    }
+
+
+    }
